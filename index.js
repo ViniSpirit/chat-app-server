ed198d017841a1e6a10212774008d60871cb362a
Marcos Vinicius <marcosvims@gmail.com>
2021-11-19 18:19:01 -0300

    cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 const server = http.createServer(app)
 
-// app.use(cors())
+app.use(cors())
 app.use(router)
 
 const endpoint = "https://kind-payne-b272f1.netlify.app"
