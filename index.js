85a536086fab2d6c0ca2f9b92d78b9f945f7360c
Marcos Vinicius <marcosvims@gmail.com>
2021-11-19 18:11:10 -0300

    test

--- index.js
+++ index.js
@@ -11,12 +11,17 @@ const PORT = process.env.PORT || 5000
 const app = express()
 const server = http.createServer(app)
 
-app.use(cors())
+// app.use(cors())
 app.use(router)
 
-const endpoint = "https://upbeat-lamport-0b2335.netlify.app"
+const endpoint = "https://kind-payne-b272f1.netlify.app"
 
-const io = socketio(server)
+const io = socketio(server, {
+  cors: {
+    origin: endpoint,
+    methods: ["GET", "POST"],
+  },
+})
 
 io.on("connection", (socket) => {
   socket.on("join", ({ name, room }, callback) => {
